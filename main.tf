dd0df2fa21df84eb3d235349fcc01192b145367c
TijmenHenrich <tijmen.henrich@visma.com>
2024-03-07 11:46:16 +0100

    d

--- main.tf
+++ main.tf
@@ -74,16 +74,6 @@ module "eks" {
       max_size     = 3
       desired_size = 2
     }
-
-    two = {
-      name = "node-group-2"
-
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
   }
 }
 
@@ -114,3 +104,33 @@ resource "aws_eks_addon" "ebs-csi" {
     "terraform" = "true"
   }
 }
+
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd-${var.environment}"
+  }
+}
+
+resource "helm_release" "argocd-staging" {
+  name       = "argocd-staging"
+  chart      = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  version    = "5.27.3"
+  namespace  = "argocd-staging"
+  timeout    = "1200"
+  values     = [templatefile("./argocd/install.yaml", {})]
+}
+resource "null_resource" "password" {
+  provisioner "local-exec" {
+    working_dir = "./argocd"
+    command     = "kubectl -n argocd-staging get secret argocd-initial-admin-secret -o jsonpath={.data.password} | base64 -d > argocd-login.txt"
+  }
+}
+
+resource "null_resource" "del-argo-pass" {
+  depends_on = [null_resource.password]
+  provisioner "local-exec" {
+    command = "kubectl -n argocd-staging delete secret argocd-initial-admin-secret"
+  }
+}
+

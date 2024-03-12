2241192712556d41dcb7b116ad8d825419e8a2c1
TijmenHenrich <tijmen.henrich@visma.com>
2024-03-12 11:03:34 +0100

    ss

--- 2_eks.tf
+++ 2_eks.tf
@@ -26,22 +26,14 @@ module "eks" {
   }
 }
 
-data "aws_eks_cluster" "default" {
-  name = module.eks.cluster_id
-}
-
-data "aws_eks_cluster_auth" "default" {
-  name = module.eks.cluster_id
-}
-
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.default.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-  # token                  = data.aws_eks_cluster_auth.default.token
+  host                   = module.eks.cluster_id.default.endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_id.default.certificate_authority[0].data)
+  # token                  = module.eks.cluster_id.default.token
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
-    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.default.id]
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_id.default.id]
     command     = "aws"
   }
 }

c15fdf1e4b6b475ebd7f8e1c3830ea2c92f7fa20
TijmenHenrich <tijmen.henrich@visma.com>
2024-03-12 09:29:08 +0100

    5

---
+++ 3_argocd.tf
@@ -0,0 +1,28 @@
+resource "kubernetes_namespace" "argocd" {
+  metadata {
+    name = "argocd-${var.environment}"
+  }
+}
+
+resource "helm_release" "argocd-$${var.environment}" {
+  name       = "argocd-${var.environment}"
+  chart      = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  version    = "5.27.3"
+  namespace  = "argocd-${var.environment}"
+  timeout    = "1200"
+  values     = [templatefile("./argocd/install.yaml", {})]
+}
+resource "null_resource" "password" {
+  provisioner "local-exec" {
+    working_dir = "./argocd"
+    command     = "kubectl -n argocd-${var.environment} get secret argocd-initial-admin-secret -o jsonpath={.data.password} | base64 -d > argocd-login.txt"
+  }
+}
+
+resource "null_resource" "del-argo-pass" {
+  depends_on = [null_resource.password]
+  provisioner "local-exec" {
+    command = "kubectl -n argocd-${var.environment} delete secret argocd-initial-admin-secret"
+  }
+}

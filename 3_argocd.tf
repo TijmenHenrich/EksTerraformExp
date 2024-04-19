51fc2e378b6d9024e8a319c86f66d81147c2a779
TijmenHenrich <tijmen.henrich@visma.com>
2024-04-19 16:09:28 +0200

    weekend

--- 3_argocd.tf
+++ 3_argocd.tf
@@ -5,10 +5,11 @@ resource "kubernetes_namespace" "argocd" {
 }
 
 # Create a secret version in AWS Secrets Manager
-resource "random_password" "argocd_admin_password" {
-  length           = 20
-  special          = true
-  override_special = "!@#"
+data "aws_secretsmanager_random_password" "argocd_admin_password" { 
+  password_length = 20 
+  exclude_numbers = false 
+  exclude_punctuation = true 
+  include_space = false 
 }
 
 # Set the random pass as the secret value in AWS Secrets Manager
@@ -17,9 +18,13 @@ resource  "aws_secretsmanager_secret" "argocd_admin_password_secret" {
 }
 
 # Fetch the password from AWS Secrets Manager
-data "aws_secretsmanager_secret_version" "argocd_admin_password_version" {
+resource "aws_secretsmanager_secret_version" "argocd_admin_password_version" {
   secret_id = aws_secretsmanager_secret.argocd_admin_password_secret.id
   secret_string = random_password.argocd_admin_password.result
+}
+
+data "aws_secretsmanager_secret_version" "argocd_admin_password_data" {
+  secret_id = aws_secretsmanager_secret.argocd_admin_password_secret.id
 }
 
 resource "helm_release" "argocd" {
@@ -30,28 +35,15 @@ resource "helm_release" "argocd" {
   namespace  = "argocd-${var.environment}"
   timeout    = "1200"
   values     = [templatefile("argocd/install.yaml", {
-    argocd_admin_password = data.aws_secretsmanager_secret_version.argocd_admin_password_version.secret_string
+    argocd_admin_password = data.aws_secretsmanager_secret_version.argocd_admin_password_data.secret_string
   })]
-}
-
-# Query AWS for Load Balancers created by ArgoCD
-data "aws_lb" "argocd_lbs" {
-  tags = {
-    "kubernetes.io/service-name" = "argocd-${var.environment}/argo-app-server" # Load Balancer tag
-    #kubernetes.io/service-name" = "argocd/argocd-dev/argo-test-app-server"  # Adjust the tag values as per your ArgoCD configuration
-  }
-}
-
-# Get the Load Balancer details using the ARN
-data "aws_lb" "argocd_lb" {
-  arn  = data.aws_lb.argocd_lbs.arn
 }
 
 # Exposed ArgoCD API - authenticated using `username`/`password`
 provider "argocd" {
   server_addr = data.aws_lb.argocd_lb.dns_name
   username    = "admin"
-  password    = data.aws_secretsmanager_secret_version.argocd_admin_password_version.secret_string
+  password    = data.aws_secretsmanager_secret_version.argocd_admin_password_data.secret_string
 }
 
 # Public Helm repository

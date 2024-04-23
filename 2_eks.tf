cdb7f2ed7cdd05ead199da973f87d54d1d58c3e9
TijmenHenrich <tijmen.henrich@visma.com>
2024-04-23 16:39:36 +0200

    sag

--- 2_eks.tf
+++ 2_eks.tf
@@ -54,6 +54,7 @@ resource "aws_eks_addon" "ebs-csi" {
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_name
+  depends_on = [ module.eks.cluster_name  ]
 }
 
 data "aws_eks_cluster_auth" "cluster" {

ef6ec44a7aab8602e41623bb59f474706ee8b244
TijmenHenrich <tijmen.henrich@visma.com>
2024-04-30 10:02:52 +0200

    dex config and sg added

--- 3_argocd.tf
+++ 3_argocd.tf
@@ -1,8 +1,28 @@
+# Define the security group
+resource "aws_security_group" "argocd_sg" {
+  name        = "argocd_sg"
+  description = "Security group allowing traffic from two specific IP addresses, vco office and vco vpn"
+
+# Inbound rule allowing LB access from two specific IP addresses
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["157.97.112.130/32", "149.11.201.58/32"]
+  }
+}
+
+# Retrieve the ARNs of the existing resources
+data "aws_security_group" "example_sg" {
+  name = "argocd_sg"
+}
+
 resource "kubernetes_namespace" "argocd" {
   metadata {
     name = "argocd-${var.environment}"
   }
 }
+
 
 # Create a secret version in AWS Secrets Manager
 data "aws_secretsmanager_random_password" "argocd_admin_password" { 
@@ -48,10 +68,14 @@ data "aws_lb" "argocd_lb" {
   }
 }
 
+# Attach the security group to the network load balancer
+resource "aws_lb_target_group_attachment" "attach_argocd_sg_to_lb" {
+  target_group_arn = data.aws_lb.example_lb.arn
+  target_id        = data.aws_security_group.example_sg.id
+}
 
 # Exposed ArgoCD API - authenticated using `username`/`password`
 provider "argocd" {
-  depends_on = [ data.aws_lb.argocd_lb.dns_name ]
   server_addr = data.aws_lb.argocd_lb.dns_name
   username    = "admin"
   password    = data.aws_secretsmanager_secret_version.argocd_admin_password_data.secret_string

c16d401451aed32dce7041e1c4c47b10ddb64c18
TijmenHenrich <tijmen.henrich@visma.com>
2024-05-06 09:27:19 +0200

    sg names

--- 3_argocd.tf
+++ 3_argocd.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "argocd_sg" {
 }
 
 # Retrieve the ARNs of the existing resources
-data "aws_security_group" "example_sg" {
+data "aws_security_group" "argocd_sg" {
   name = "argocd_sg"
 }
 
@@ -70,8 +70,8 @@ data "aws_lb" "argocd_lb" {
 
 # Attach the security group to the network load balancer
 resource "aws_lb_target_group_attachment" "attach_argocd_sg_to_lb" {
-  target_group_arn = data.aws_lb.example_lb.arn
-  target_id        = data.aws_security_group.example_sg.id
+  target_group_arn = data.aws_lb.aws_lb.arn
+  target_id        = data.aws_security_group.argocd_sg.id
 }
 
 # Exposed ArgoCD API - authenticated using `username`/`password`

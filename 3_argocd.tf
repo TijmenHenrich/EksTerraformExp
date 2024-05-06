7c7b63adf56d4b58d806762668fb0a147b149c5a
TijmenHenrich <tijmen.henrich@visma.com>
2024-05-06 09:29:48 +0200

    lb typo

--- 3_argocd.tf
+++ 3_argocd.tf
@@ -70,7 +70,7 @@ data "aws_lb" "argocd_lb" {
 
 # Attach the security group to the network load balancer
 resource "aws_lb_target_group_attachment" "attach_argocd_sg_to_lb" {
-  target_group_arn = data.aws_lb.aws_lb.arn
+  target_group_arn = data.aws_lb.argocd_lb.arn
   target_id        = data.aws_security_group.argocd_sg.id
 }
 
